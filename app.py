8dc287d2dba491e47a20954c576382fd6b491fdb
SOURASHIS SARKAR <sourashissarkar3@gmail.com>
2024-03-03 22:27:47 +0530

    first commit

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('front.html')
+    return render_template('front2.html')
 @app.route('/home')
 def home():
     return render_template('bot2.html')
@@ -45,6 +45,28 @@ def generate_response(user_message):
        else:
            responses = solution(user_message)
     return responses
+@app.route("/voice")
+def voice():
+    return render_template('bot copy.html')
+
+@app.route('/process_voice', methods=['POST'])
+def process_voice():
+    user_message = request.form['user_message']
+    bot_response = generate_response(user_message)  # Implement your bot logic here
+    return jsonify({'response': bot_response})
+def generate_response(user_message):
+    # Implement your chatbot logic here
+    if "your name" in user_message or user_message=="hi" or "hello" in user_message or "hey" in user_message:
+        responses=" Hi , My name is Rahu"
+    elif "who made you" in user_message or "your architect" in user_message or "your maker" in user_message:
+        responses="I was created by Sourashis Sarkar"
+    else:
+       if solution(user_message)=="None":
+           responses="I don't understand"
+       else:
+           responses = solution(user_message)
+    return responses
+
  
 if __name__ == '__main__':
     app.run(debug=True)
